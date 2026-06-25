081e5749b0067363cca1ef628125bd64d97b779e
Yandris Rivera Torres <yan.2005dris@gmail.com>
2026-06-25 14:34:39 -0500

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,7 +16,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://app2.dihm-muertos.site',
+    ],
 
     'allowed_origins_patterns' => [],
 
