94b1b512d973469897e8a1efa069e1abb07b03e6
Yandris Rivera Torres <yan.2005dris@gmail.com>
2026-07-26 18:05:30 -0500

    fix(config): support FRONTEND_BASE_URL environment variable in app.frontend_url config

--- backend/config/app.php
+++ backend/config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3006'),
+    'frontend_url' => env('FRONTEND_URL', env('FRONTEND_BASE_URL', env('APP_URL', 'http://localhost:3006'))),
 
     /*
     |--------------------------------------------------------------------------
