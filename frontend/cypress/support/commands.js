1ec02d189976ed59132e49d67638ddf390a6d291
Yandris Rivera Torres <yan.2005dris@gmail.com>
2026-07-27 01:13:28 -0500

    fix(e2e): scope login submit-button selector to the login form

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -2,7 +2,7 @@ Cypress.Commands.add('login', (email, password) => {
   cy.visit('/#/login');
   cy.get('#email').type(email);
   cy.get('#password').type(password);
-  cy.get('button[type="submit"]').click();
+  cy.get('#login-form button[type="submit"]').click();
   cy.url().should('include', '/#/dashboard');
 });
 
